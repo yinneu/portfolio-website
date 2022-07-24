c74a38081a83edfc6e056ea7cbdc9ddb159cb53c
yerimYeon <99879845+yinneu@users.noreply.github.com>
2022-07-25 00:27:31 +0900

    JS : Arrow up

--- main.js
+++ main.js
@@ -15,32 +15,45 @@ document.addEventListener("scroll", () => {
 //Handle scolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (e) => {
-   const target = e.target;
-   const link = target.dataset.link
-   if (link == null){
-    return;
-   }
+  const target = e.target;
+  const link = target.dataset.link
+  if (link == null){
+  return;
+  }
 
-   scrollIntoView(link);
+  scrollIntoView(link);
 });
 
 
 //Handle click on "contact me" button on home
 const contactBtn = document.querySelector('.home__contact');
 contactBtn.addEventListener('click', () => {
-    scrollIntoView('#contact');
+  scrollIntoView('#contact');
 });
 
 
-
-//스크롤 다운시 home 투명도
 //Make home slowly fade to transparent as the window scrolls down
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-
   home.style.opacity = 1 - window.scrollY / homeHeight
+});
 
+
+//Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if(window.scrollY > homeHeight /2){
+    arrowUp.classList.add('arrow--visible');
+  } else {
+    arrowUp.classList.remove('arrow--visible');
+  }
+});
+
+
+//Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
 });
 
 
